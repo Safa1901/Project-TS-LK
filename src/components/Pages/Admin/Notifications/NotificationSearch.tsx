0ba3865efa4dbe67834cd5b51b838cabfd67902a
Anastasiya <selivanovali87@gmail.com>
2024-03-29 00:36:06 +0300

    установлен дата-пикер

--- src/components/Pages/Admin/Notifications/NotificationSearch.tsx
+++ src/components/Pages/Admin/Notifications/NotificationSearch.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import TextField from '@material-ui/core/TextField';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import styles from '../styles/container.module.scss'
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
@@ -10,7 +14,7 @@ const useStyles = makeStyles((theme: Theme) =>
                 margin: theme.spacing(1),
                 width: '25ch',
             },
-        }
+        },
     })
 )
 
@@ -22,7 +26,14 @@ export const NotificationSearch = () => {
             <form className={classes.root} noValidate autoComplete="off">
                 <TextField id="outlined-basic" label="№ объекта" variant="outlined" />
                 <TextField id="outlined-basic" label="Телофон" variant="outlined" />
-                
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <div className={styles.input_date}>
+                        <DatePicker />
+                    </div>
+                    <div className={styles.input_date}>
+                        <DatePicker />
+                    </div>
+                </LocalizationProvider>
             </form>
         </div>
     )
